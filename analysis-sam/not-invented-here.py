d50d4b7ca8b222b4ceef937786b3b5637702b31c
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2020-03-11 19:19:08 +0100

    adds bandpower calc and ratios

--- analysis-sam/not-invented-here.py
+++ analysis-sam/not-invented-here.py
@@ -18,7 +18,9 @@ import seaborn as sns
 from functools import reduce
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from functional import seq
 from scipy import signal
+from itertools import combinations 
 
 #%matplotlib inline
 
@@ -28,60 +30,11 @@ from scipy import signal
 
 # <codecell>
 
-def bandpower(data, sf, band, window_sec=None, relative=False):
-    """Compute the average power of the signal x in a specific frequency band.
-
-    Parameters
-    ----------
-    data : 1d-array
-        Input signal in the time-domain.
-    sf : float
-        Sampling frequency of the data.
-    band : list
-        Lower and upper frequencies of the band of interest.
-    window_sec : float
-        Length of each window in seconds.
-        If None, window_sec = (1 / min(band)) * 2
-    relative : boolean
-        If True, return the relative power (= divided by the total power of the signal).
-        If False (default), return the absolute power.
-
-    Return
-    ------
-    bp : float
-        Absolute or relative band power.
-    """
-    band = np.asarray(band)
-    low, high = band
-
-    # Define window length
-    if window_sec is not None:
-        nperseg = window_sec * sf
-    else:
-        nperseg = (2 / low) * sf
-
-    # Compute the modified periodogram (Welch)
-    freqs, psd = scipy.signal.welch(data, sf, nperseg=nperseg)
-
-    # Frequency resolution
-    freq_res = freqs[1] - freqs[0]
-
-    # Find closest indices of band in frequency vector
-    idx_band = np.logical_and(freqs >= low, freqs <= high)
-
-    # Integral approximation of the spectrum using Simpson's rule.
-    bp = scipy.integrate.simps(psd[idx_band], dx=freq_res)
-
-    if relative:
-        bp /= scipy.integrate.simps(psd, dx=freq_res)
-    return bp
-
-# <codecell>
-
 def bandpower(data, sf, band, method='welch', window_sec=None, relative=False):
     """Compute the average power of the signal x in a specific frequency band.
 
     Requires MNE-Python >= 0.14.
+    Source: https://raphaelvallat.com/bandpower.html
 
     Parameters
     ----------
@@ -108,10 +61,11 @@ def bandpower(data, sf, band, method='welch', window_sec=None, relative=False):
     from scipy.signal import welch
     from scipy.integrate import simps
     from mne.time_frequency import psd_array_multitaper
+    
 
     band = np.asarray(band)
     low, high = band
-
+    
     # Compute the modified periodogram (Welch)
     if method == 'welch':
         if window_sec is not None:
@@ -119,6 +73,10 @@ def bandpower(data, sf, band, method='welch', window_sec=None, relative=False):
         else:
             nperseg = (2 / low) * sf
 
+        if data.shape[0] < nperseg:
+            return np.NaN
+            
+        
         freqs, psd = welch(data, sf, nperseg=nperseg)
 
     elif method == 'multitaper':
@@ -136,7 +94,17 @@ def bandpower(data, sf, band, method='welch', window_sec=None, relative=False):
 
     if relative:
         bp /= simps(psd, dx=freq_res)
+        
     return bp
+
+# <codecell>
+
+def index_to_time(x, time_index, step_size=1):
+    if (x < 0 or x * step_size >= len(time_index)):
+        return ''
+    
+    seconds = time_index[int(x*step_size)].total_seconds()
+    return f"{int(seconds/60)}\' {seconds/60:.2f}\""
 
 # <codecell>
 
@@ -151,9 +119,10 @@ def get_config_value(config, *args):
 
 cfg = {
     'paths': {
+        'base': '../../data/AlphaTheta',
         'subjects': {
             'sam': {
-                'prefix': '../data/AlphaTheta/sam-AlphaTheta/',
+                'prefix': '/sam-AlphaTheta',
                 'recordings': {
                     'baseline': [
                         '20200304-144100',
@@ -166,6 +135,7 @@ cfg = {
             }
         }
     },
+    'sampling_frequency': 300,
     'bands': {
         'gamma': [40, 100],
         'beta':  [12, 40],
@@ -179,7 +149,25 @@ cfg = {
 
 subject_paths = get_config_value(cfg, 'paths', 'subjects', 'sam')
 
-baseline = pd.read_pickle(f"{subject_paths['prefix']}/offline/{get_config_value(subject_paths, 'recordings', 'baseline')[0]}-raw.pcl")
+baseline = pd.read_pickle(f"{cfg['paths']['base']}/{subject_paths['prefix']}/offline/{get_config_value(subject_paths, 'recordings', 'baseline')[0]}-raw.pcl")
+meditation =  pd.read_pickle(f"{cfg['paths']['base']}/{subject_paths['prefix']}/offline/{get_config_value(subject_paths, 'recordings', 'meditation')[0]}-raw.pcl")
+
+# <codecell>
+
+def load_signal_data(data_type, subject='sam', number=0):
+    subject_paths = get_config_value(cfg, 'paths', 'subjects', subject)
+    data = pd.read_pickle(f"{cfg['paths']['base']}/{subject_paths['prefix']}/offline/{get_config_value(subject_paths, 'recordings', data_type)[0]}-raw.pcl")
+    
+    _t = data['timestamps'].reshape(-1)
+    _t -= _t[0]
+
+    return pd.DataFrame(data=data['signals'],
+                              index=pd.TimedeltaIndex(_t, unit='s'), 
+                              columns=data['ch_names'])
+
+# <codecell>
+
+meditation_pd = load_signal_data('meditation')
 
 # <codecell>
 
@@ -196,12 +184,6 @@ signals_pd = pd.DataFrame(data=baseline['signals'],
 _sampling_rate=10
 fig = signals_pd.loc[::_sampling_rate, :].plot(figsize=(40, 20), use_index=True)
 
-def index_to_time(x, time_index, step_size=1):
-    if (x < 0 or x * step_size >= len(time_index)):
-        return ''
-    
-    seconds = time_index[int(x*step_size)].total_seconds()
-    return f"{int(seconds/60)}\' {seconds/60:.2f}\""
 
 fig.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: index_to_time(x, signals_pd.index, _sampling_rate)))
 fig.set_xlabel('time [s]')
@@ -211,6 +193,81 @@ fig.set_title('raw measurements of voltage for each electrode');
 # <codecell>
 
 signals_pd.columns
+
+# <codecell>
+
+signals_pd.shape
+
+# <codecell>
+
+def get_bandpower_for_electrode(signal_data, electrode='T5'):
+    time_index_of_interest = signal_data.index
+
+    bandpowers = {}
+
+    for band_name, band_range in cfg['bands'].items():
+        bandpowers[band_name] = signal_data.loc[time_index_of_interest, electrode_placement]\
+            .rolling('1s')\
+            .apply(lambda xs: bandpower(xs, cfg['sampling_frequency'], band_range))
+
+    for bn_l, bn_r in combinations(cfg['bands'].keys(), 2):
+        bandpowers[f"{bn_l} / {bn_r}"] = bandpowers[bn_l] / bandpowers[bn_r]
+        
+    return bandpowers, time_index_of_interest
+
+# <codecell>
+
+def plot_bandpowers(bandpowers, time_index_of_interest):
+    fig, axs = plt.subplots(nrows=len(bandpowers), sharex=True, figsize=(25, 15))
+    time_index_as_seconds = [t.total_seconds() for t in time_index_of_interest]
+
+    for i, (bn, bp) in enumerate(bandpowers.items()):
+        axs[i].plot(bp.reset_index(drop=True))
+        axs[i].set_ylabel(bn)
+
+    axs[0].xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: index_to_time(x, time_index_of_interest)))
+    fig.suptitle(f"Bandpower of {electrode_placement}")
+    
+    return fig
+
+# <codecell>
+
+meditation_bandpower, meditation_time_index = get_bandpower_for_electrode(meditation_pd)
+
+# <codecell>
+
+plot_bandpowers_bandpowers(meditation_bandpower, meditation_time_index);
+
+# <codecell>
+
+# TODO what type of window? this is just a window with equal weight
+# the thing with the index
+electrode_placement = 'T5'
+
+#time_index_of_interest = signals_pd.index[:5 * cfg['sampling_frequency']]
+time_index_of_interest = signals_pd.index
+
+bandpowers = {}
+    
+for band_name, band_range in cfg['bands'].items():
+    bandpowers[band_name] = signals_pd.loc[time_index_of_interest, electrode_placement]\
+        .rolling('1s')\
+        .apply(lambda xs: bandpower(xs, cfg['sampling_frequency'], band_range))
+    
+for bn_l, bn_r in combinations(cfg['bands'].keys(), 2):
+    bandpowers[f"{bn_l} / {bn_r}"] = bandpowers[bn_l] / bandpowers[bn_r]
+
+# <codecell>
+
+fig, axs = plt.subplots(nrows=len(bandpowers), sharex=True, figsize=(25, 15))
+time_index_as_seconds = [t.total_seconds() for t in time_index_of_interest]
+
+for i, (bn, bp) in enumerate(bandpowers.items()):
+    axs[i].plot(bp.reset_index(drop=True))
+    axs[i].set_ylabel(bn)
+    
+axs[0].xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: index_to_time(x, time_index_of_interest)))
+fig.suptitle(f"Bandpower of {electrode_placement}");
 
 # <codecell>
 
