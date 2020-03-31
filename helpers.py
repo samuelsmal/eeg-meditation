7a7b09c0789d94127cc8d26788117059b953d9ac
Adcerr <adelie.cerrato@epfl.ch>
2020-03-31 10:12:56 +0200

    First steps with MNE package: EEG and CSD

---
+++ helpers.py
@@ -0,0 +1,217 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from functools import reduce
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from functional import seq
+from scipy import signal
+from itertools import combinations 
+from os import path
+
+
+def bandpower(data, sf, band, method='welch', window_sec=None, relative=False):
+    """Compute the average power of the signal x in a specific frequency band.
+
+    Requires MNE-Python >= 0.14.
+    Source: https://raphaelvallat.com/bandpower.html
+
+    Parameters
+    ----------
+    data : 1d-array
+      Input signal in the time-domain.
+    sf : float
+      Sampling frequency of the data.
+    band : list
+      Lower and upper frequencies of the band of interest.
+    method : string
+      Periodogram method: 'welch' or 'multitaper'
+    window_sec : float
+      Length of each window in seconds. Useful only if method == 'welch'.
+      If None, window_sec = (1 / min(band)) * 2.
+    relative : boolean
+      If True, return the relative power (= divided by the total power of the signal).
+      If False (default), return the absolute power.
+
+    Return
+    ------
+    bp : float
+      Absolute or relative band power.
+    """
+    from scipy.signal import welch
+    from scipy.integrate import simps
+    from mne.time_frequency import psd_array_multitaper
+    
+
+    band = np.asarray(band)
+    low, high = band
+    
+    # Compute the modified periodogram (Welch)
+    if method == 'welch':
+        if window_sec is not None:
+            nperseg = window_sec * sf
+        else:
+            nperseg = (2 / low) * sf
+
+        if data.shape[0] < nperseg:
+            return np.NaN
+            
+        
+        freqs, psd = welch(data, sf, nperseg=nperseg)
+
+    elif method == 'multitaper':
+        psd, freqs = psd_array_multitaper(data, sf, adaptive=True,
+                                          normalization='full', verbose=0)
+
+    # Frequency resolution
+    freq_res = freqs[1] - freqs[0]
+
+    # Find index of band in frequency vector
+    idx_band = np.logical_and(freqs >= low, freqs <= high)
+
+    # Integral approximation of the spectrum using parabola (Simpson's rule)
+    bp = simps(psd[idx_band], dx=freq_res)
+
+    if relative:
+        bp /= simps(psd, dx=freq_res)
+        
+    return bp
+
+def get_bandpower_for_electrode(signal_data, electrode, config, window_size='1s'):
+    """Calculates the bandpower for the given electrode
+    
+    Note that this will take some time... I suggest that you only use a part of the signal to try it out.
+    
+    Parameters
+    ----------
+    signal_data: 2d pandas dataframe
+        raw signal data, indexed by a timedeltaindex (or any other time-based index)
+    electrode: string
+        name of the electrode of interest
+    config: dict
+        dict of config parameters
+    window_size: string
+        size of rolling window
+        
+    Returns
+    -------
+    a new pandas dataframe of the bandpowers, in addition all ration combinations are listed as well
+    """
+    bandpowers = {}
+
+    for band_name, band_range in config['bands'].items():
+        bandpowers[band_name] = signal_data.loc[:, electrode]\
+            .rolling(window_size)\
+            .apply(lambda xs: bandpower(xs, config['sampling_frequency'], band_range))
+
+    # compute all different ratios
+    for bn_l, bn_r in combinations(cfg['bands'].keys(), 2):
+        bandpowers[f"{bn_l} / {bn_r}"] = bandpowers[bn_l] / bandpowers[bn_r]
+        
+    return bandpowers 
+
+def aggregate_bandpower(baseline, signal):
+    aggregated_fns = ['mean', 'median', 'min', 'max']
+    aggregated_power = pd.DataFrame(index=pd.MultiIndex.from_product([list(baseline.keys()), ['baseline', 'meditation']]),
+                                    columns=aggregated_fns)
+
+    for band, power in baseline.items():
+        aggregated_power.loc[(band, "baseline"), :] = power.agg(aggregated_fns)
+
+
+    for band, power in signal.items():
+        aggregated_power.loc[(band, 'meditation'), :] = power.agg(aggregated_fns)
+
+
+    return aggregated_power
+
+def load_signal_data(data_type, config, subject='sam', recording=0, remove_references=True):
+    """loads the data and returns a pandas dataframe 
+    
+    Parameters
+    ----------
+    data_type : string
+      type of the data, right now two options are valid: `baseline` or `meditation`
+    subject: string
+      name of the subject
+    recording: int
+      number of recording, if you have multiple of same type and subject
+      
+    Returns
+    -------
+    a pandas dataframe, timedeltaindexed of the raw signals
+    """
+    subject_paths = get_config_value(config, 'paths', 'subjects', subject)
+    data = pd.read_pickle(f"{config['paths']['base']}/{subject_paths['prefix']}/offline/{get_config_value(subject_paths, 'recordings', data_type)[recording]}-raw.pcl")
+    
+    _t = data['timestamps'].reshape(-1)
+    _t -= _t[0]
+
+    signal = pd.DataFrame(data=data['signals'], 
+                          index=pd.TimedeltaIndex(_t, unit='s'), 
+                          columns=data['ch_names'])\
+               .drop(columns=config['columns_to_remove'])
+    
+    return signal.loc[signal.index[config['default_signal_crop']], :]
+
+def plot_bandpowers(bandpowers, electrode):
+    fig, axs = plt.subplots(nrows=len(bandpowers), sharex=True, figsize=(25, 15))
+    time_index = list(bandpowers.values())[0].index
+    time_index_as_seconds = [t.total_seconds() for t in time_index]
+
+    for i, (bn, bp) in enumerate(bandpowers.items()):
+        axs[i].plot(bp.reset_index(drop=True))
+        axs[i].set_ylabel(bn)
+
+    axs[0].xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: index_to_time(x, time_index)))
+    fig.suptitle(f"Bandpower of {electrode}")
+    
+    return fig
+
+
+def plot_raw_signal(signal_pd, sampling=10):
+    """
+    Parameters
+    ----------
+    signal_pd: 2d pandas dataframe
+        long-format (a column for each electrode)
+    sampling: int
+        step size of data points used for plotting
+    
+    Returns
+    -------
+    a figure of the plot
+    
+    """
+    
+    fig, axs = plt.subplots(nrows=signal_pd.shape[1], figsize=(40, 1.4 * signal_pd.shape[1]), sharex=True)
+    for channel_id, channel in enumerate(signal_pd.columns):
+        d = signal_pd.loc[::sampling, channel]
+        sns.lineplot(data=d.reset_index(drop=True), ax=axs[channel_id])
+        axs[channel_id].set_ylabel(channel)
+
+    axs[-1].set_xlabel('time [ms]')
+    axs[-1].xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: index_to_time(x, d.index)))
+    
+    fig.suptitle("Raw signal recording")
+    
+    return fig
+
+def index_to_time(x, time_index, step_size=1):
+    """Helper function to add the axis labels"""
+    if (x < 0 or x * step_size >= len(time_index)):
+        return ''
+    
+    seconds = time_index[int(x*step_size)].total_seconds()
+    return f"{int(seconds/60)}\' {seconds/60:.2f}\""
+
+def get_config_value(config, *args):
+    """Helper to get read the config"""
+    return reduce(lambda cfg, val: cfg[val], args, config)
+                          
+def get_channelsList(config, subject='adelie'):
+    subject_config = config['paths']['subjects'][subject]
+    file_path = path.join(config['paths']['base'], subject_config['prefix'],  subject_config['channels'])
+    with open(file_path, 'r') as channels_file:
+        all_channels = channels_file.read()
+    return [channel for channel in all_channels.split('\n') if channel not in config['columns_to_remove']]
