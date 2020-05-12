c2f9991081e0288004e7059461a8c05bd0cfef5b
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2020-05-12 15:42:04 +0200

    moves neurodecode protocols to a better place

---
+++ neurodecode_protocols/AlphaTheta/sam-AlphaTheta/config_offline_sam-AlphaTheta.py
@@ -0,0 +1,12 @@
+DATA_PATH = ''
+
+TRIGGER_DEVICE = 'SOFTWARE'
+TRIGGER_FILE = r'/home/sam/proj/epfl/eeg-meditation/NeuroDecode/triggers/triggerdef_16.ini'
+
+START_VOICE_FILE = r'/home/sam/proj/epfl/eeg-meditation/nfme/meditation_sounds/startRecordingVoice.mp3'
+END_VOICE_FILE = r'/home/sam/proj/epfl/eeg-meditation/nfme/meditation_sounds//EndRecordingVoice.mp3'
+
+SCREEN_SIZE = (1920, 1200)
+SCREEN_POS = (0,0)
+
+GLOBAL_TIME = 0.5*60
