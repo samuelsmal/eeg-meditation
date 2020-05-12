55e48d91c76d9cd520739039e5328ec667a50ec9
Samuel von Baussnern <savoba@protonmail.ch>
2020-05-12 14:42:40 +0200

    new protocols

---
+++ nfme/neurodecode_protocols/new_scripts/AlphaTheta/sam-AlphaTheta/config_offline_sam-AlphaTheta.py
@@ -0,0 +1,15 @@
+DATA_PATH = ''
+
+TRIGGER_DEVICE = 'SOFTWARE'
+TRIGGER_FILE = r'/home/sam/proj/epfl/eeg-meditation/NeuroDecode/triggers/triggerdef_16.ini'
+#TRIGGER_FILE = r'C:\Users\adesvachez\git\NeuroDecode\neurodecode\triggers\triggerdef_16.ini'
+
+START_VOICE_FILE = r'/home/sam/proj/epfl/eeg-meditation/meditation_sounds/startRecordingVoice.mp3'
+#START_VOICE_FILE = r'C:\Users\adeli\neurodecode\meditation_sounds\startRecordingVoice.mp3'
+END_VOICE_FILE = r'/home/sam/proj/epfl/eeg-meditation/meditation_sounds/EndRecordingVoice.mp3'
+#END_VOICE_FILE = r'C:\Users\adeli\neurodecode\meditation_sounds\EndRecordingVoice.mp3'
+
+SCREEN_SIZE = (1920, 1200)
+SCREEN_POS = (0,0)
+
+GLOBAL_TIME = 0.5*60
---
+++ nfme/neurodecode_protocols/new_scripts/AlphaTheta/sam-AlphaTheta/config_online_sam-AlphaTheta.py
@@ -0,0 +1,36 @@
+#----------------------------------------------------------------------
+# Parameters to define
+#----------------------------------------------------------------------
+DATA_PATH = ''
+
+SPATIAL_FILTER = None
+SPATIAL_CHANNELS = ['Fz', 'F3', 'F4', 'F7', 'F8', 'Cz', 'C3', 'C4', 'P3', 'Pz', 'P4']
+
+ALPHA_CHANNELS = ['Pz']
+ALPHA_REF = 239550
+ALPHA_THR = 376355
+
+ALPHA_FREQ = {'min': 8, 'max': 12}
+THETA_FREQ = {'min': 4, 'max': 8}
+
+THETA_CHANNELS = ['Pz']
+THETA_REF = 702870
+THETA_THR = 982914
+
+STREAMBUFFER = 2                   # Stream buffer [sec]
+WINDOWSIZE = 2                     # window length of acquired data when calling get_window [sec]
+
+TIMER_SLEEP = 0.01 * 60
+
+GLOBAL_TIME = 300
+
+NJOBS = 8                          # For multicore PSD processing
+
+ALPHA_FEEDBACK_PATH = r'C:\Users\adesvachez\Documents\meditation_sounds\babbling-brook.wav'
+THETA_FEEDBACK_PATH = r'C:\Users\adesvachez\Documents\meditation_sounds\ocean-waves.wav'
+
+ALPHA_SUP_FB_PATH = r'C:\Users\adesvachez\Documents\meditation_sounds\A-Tone.wav'
+THETA_SUP_FB_PATH = r'C:\Users\adesvachez\Documents\meditation_sounds\ZenBuddhistTempleBell.wav'
+
+HARMONIC_SOUND_PATH = '/home/sam/Downloads/Harmonisch.wav'
+UNHARMONIC_SOUND_PATH = '/home/sam/Downloads/Unharmonisch.wav'
